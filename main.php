4cc4492b3ae9bc48e2a88d868a38e123ee42a6c5
Miguel Márquez <mmarquez@alternetica.com>
2018-04-29 23:27:48 -0500

    Reduce code to migration

--- main.php
+++ main.php
@@ -4,24 +4,18 @@
 *
 */
 // Integrator Class
-require_once "Integrator.php";
+// Integrator Class
+require_once 'Integrator.php';
+// Composer Autoload
+require_once 'vendor\autoload.php';
+// API inDataLogic
+require_once 'api_tcintegrator\indata_ws\inDataLogic.php';
 
 // New Resource Integrator
-$resource = new Integrator();
-$fecha = date('YmdHi');
+$indata = new Integrator();
 
 /***  Menu de Bienvenida ***/ 
 echo "Bienvenido a Integrator\n";
-echo "Desesa realizar la importacion? Escriba 'SI' para continuar: ";
-$handle = fopen ("php://stdin","r");
-$line = fgets($handle);
-if(trim($line) != 'SI'){
-    echo "\n Saliendo!\n";
-    exit;
-}
-fclose($handle);
-
-
 echo "\n"; 
 echo "Selecciones la tabla que desea importar\n";
 echo "1. Clientes\n";
@@ -30,7 +24,7 @@ echo "3. Cartera\n";
 echo "4. Productos\n";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
-$name = "\/".$fecha.'.csv';
+
 
 switch ($line) {
     case 1:
@@ -38,42 +32,10 @@ switch ($line) {
         $line = fgets($handle);
         if(trim($line) == 'SI'){
             echo "Ejecutando tarea Clientes..!\n";
-
-            // CSV Path/Name
-            $file_dir = getcwd().'/public/tmp/';
             
             try {
-
-                echo $file_name = getcwd().'/public/' . 'cliente.CSV';
-                $folder = 'clientes';
-                // Valida Ruta, Archivo y Lectura
-                $resource->ValidatePath($file_name);
-
-                // Ultimo CSV Generado hasta la Fecha
-                echo $last = $resource->LastCSV($file_dir, $folder);
-                echo "\n";
-                echo $file_tmp = $file_dir.$folder."/".$last;
-                echo "\n";
-                $file_last = $resource->ReadCSV($file_tmp);
-
-                // Copia de CSV Nuevo 
-                echo $resource->CopyCSV($file_name, $folder, $fecha);
-
-                // Ultimo CSV Generado (Copia Nuevo)
-                echo $new = $resource->LastCSV($file_dir, $folder);
-                echo "\n";
-                echo $file_tmp = $file_dir.$folder."/".$new;
-                echo "\n";
-                $file_new = $resource->ReadCSV($file_tmp);
                 
-                print_r($arreglo = $resource->CompareCSV($file_last, $file_new));
-
-                //$resource->CreateCSV($arreglo, $file_name, $name);
-                
-                // //Creando Archivo Base para Procesarlo
-                // print_r($resource->CreateCSV($resource->ReadCSV($file_name), $file_name, $name));
-                // //Execute Method for inDataLogic
-                // $resource->ClientInData($resource->ReadCSV($client_file));
+                $indata->MigraCSV();                
           
             } catch (Exception $e) {
           
